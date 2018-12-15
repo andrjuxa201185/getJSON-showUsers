f9831e547daa0476cfe2b3cb3b07f99968a475fe
andrey podgorniy <andrjuxa201185@gmai.com>
2018-12-15 19:12:07 +0200

    well done

--- script/js.js
+++ script/js.js
@@ -1,37 +1,64 @@
-const getData = () => {
-    const xhr = new XMLHttpRequest();
-    const userName = "andrjuxa201185";
+document.addEventListener("DOMContentLoaded", function(){
+    const button = document.querySelector(".btn");
+    const ul = document.querySelector('ul');
+    let desc = document.querySelector('.description');
+    const closeDesc = document.querySelector('.description .close');
 
-    // xhr.open("GET", '../data/data.txt');
-    xhr.open("GET", `http://api.github.com/users/${userName}`);
-    xhr.send();
+    const getUsers = () => {
+        const xhr = new XMLHttpRequest();
     
-    xhr.addEventListener("readystatechange", () => {
-        if (xhr.readyState != 4) return;
+        xhr.open("GET", `../data/users.json`);
+        xhr.send();
+        xhr.addEventListener("readystatechange", () => {
+            if (xhr.readyState != 4) return;
+        
+            if (xhr.status !== 200) {
+                console.warn(`error ${xhr.status}`);
+            } else {
+                const users = JSON.parse(xhr.responseText);
+                showUsers(users);
+            }
+        });
+    };
     
-        if (xhr.status !== 200) {
-            console.warn(`error ${xhr.status}`);
-        } else {
-            const phones = JSON.parse(xhr.responseText);
-            console.log(phones);
-            // createPhone(phones[0]);
+    const showUsers = (users) => {    
+        for (let i = 0; i < users.length; i++) {
+            let li = document.createElement('li');
+            li.innerHTML = users[i].name;
+            li.setAttribute('data-gender', users[i].gender);
+            li.setAttribute('data-job', users[i].job);
+            li.setAttribute('data-name', users[i].name);
+            ul.appendChild(li);
+        }
+    };
+    
+    const showInfo = (li) => {
+        let job = li.getAttribute('data-job');
+        let gender = li.getAttribute('data-gender');
+        let name = li.getAttribute('data-name');
+        desc.querySelector('.job span').innerHTML = job;
+        desc.querySelector('.gender span').innerHTML = gender;
+        desc.querySelector('.name span').innerHTML = name;
+        desc.classList.remove('d-none');
+        li.appendChild(desc);
+    };
+
+
+    button.addEventListener("click", getUsers);
+
+    ul.addEventListener("click", (e) => {
+        const elem = e.target;
+        if (elem.tagName == "LI"){
+            showInfo(elem);
         }
     });
-};
 
-const showInfo = ({avatarUrl, name}) => {
-    
-}
-
-// const createPhone = ({imageUrl, name}) => {
-//     document.body.insertAdjacentHTML("afterend", `<div><img src="${imageUrl}"></div><h1>${name}</h1>`);
-// };
-
-document.addEventListener("DOMContentLoaded", function(){
-    const btn = document.querySelector(".btn");
-    btn.addEventListener("click", getData);
-    console.log(getData);
+    closeDesc.addEventListener("click", function() {
+        this.parentElement.classList.add("d-none");
+    });    
 });
+
+
 
 
 
